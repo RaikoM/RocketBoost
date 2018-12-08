c6bbd2c2537b61f2a5d16dee3490666b3c1480cb
Raiko Mereküla <raiko.merekyla@testreel.com>
2018-12-08 16:07:27 +0200

    added thrust and rotate variables

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,6 +6,8 @@ public class Rocket : MonoBehaviour {
 
 	Rigidbody rigidbody;
 	AudioSource audioSource;
+	[SerializeField] float rcsThrust = 100f;
+	[SerializeField] float mainThrust = 50f;
 
 	// Use this for initialization
 	void Start () {
@@ -20,16 +22,24 @@ public class Rocket : MonoBehaviour {
 	}
 
 	private void Rotate() {
+		rigidbody.freezeRotation = true; // take manual control of rotation
+
+		// var rcsThrust = 100f;
+		float rotationThisFrame = rcsThrust * Time.deltaTime;
+
 		if (Input.GetKey(KeyCode.A)){
-			transform.Rotate(Vector3.forward);
+			transform.Rotate(Vector3.forward * rotationThisFrame);
 		} else if (Input.GetKey(KeyCode.D)){
-			transform.Rotate(-Vector3.forward);
+			transform.Rotate(-Vector3.forward * rotationThisFrame);
 		}
+
+		rigidbody.freezeRotation = false; // resume physics control of rotation
 	}
 
 	private void Thrust() {
+		float thrustThisFrame = mainThrust * Time.deltaTime;
 		if (Input.GetKey(KeyCode.Space)){
-			rigidbody.AddRelativeForce(Vector3.up);
+			rigidbody.AddRelativeForce(Vector3.up * thrustThisFrame);
 			if (!audioSource.isPlaying) {
 				audioSource.Play();
 			}
